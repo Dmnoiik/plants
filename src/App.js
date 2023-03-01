05649dea40f9062d8da5a1a647fe2f2009f3b6dc
Dmnoiik <d.marszalkiewicz2000@gmail.com>
2023-03-01 10:36:25 +0100

    Added navigation component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Login from "./Components/Login/Login";
+import MainNavigation from "./Components/MainNavigation/MainNavigation";
 
 function App() {
   return (
     <div className="App">
+      <MainNavigation />
       <Login />
     </div>
   );
---
+++ src/Components/MainNavigation/MainNavigation.js
@@ -0,0 +1,6 @@
+import style from "./MainNavigation.module.css";
+const MainNavigation = () => {
+  return <div className={style["nav--main"]}>NAV</div>;
+};
+
+export default MainNavigation;
