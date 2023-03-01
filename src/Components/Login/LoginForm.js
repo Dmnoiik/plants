4f3e608db2f82db71a91f6897248ec84ad0ed693
Dmnoiik <d.marszalkiewicz2000@gmail.com>
2023-03-01 13:17:51 +0100

    Styling the login form

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,21 +1,35 @@
+import React, { useState } from "react";
 import style from "./LoginForm.module.css";
 import plantImage from "../Images/plant.jpg";
 
 const LoginForm = () => {
+  const [isActive, setNewIsActive] = useState(true);
+  const activeHandler = () => {
+    setNewIsActive((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
   return (
-    <form className={style["form--container"]}>
+    <form onSubmit={submitHandler} className={style["form--container"]}>
       <div>
-        <h2>Login</h2>
+        <h2 className={style["form--heading"]}>Login</h2>
       </div>
       <div className={style["form--toggle"]}>
-        <div>Login</div>
-        <div>Signup</div>
+        <button
+          onClick={activeHandler}
+          className={isActive ? style.active : ""}
+        >
+          Login
+        </button>
+        <button>Signup</button>
       </div>
       <div className={style["form--inputs"]}>
         <input type="text" placeholder="Email Address" />
         <input type="password" placeholder="Password" />
       </div>
-      <button>Login</button>
+      <button className={style["form--login"]}>Login</button>
     </form>
   );
 };
